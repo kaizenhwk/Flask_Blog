6a7d3a120674270df96e9760952646089909fb73
kaizenhwk <matin.mafuz@gmail.com>
2019-08-09 20:20:28 -0500

    Added app route for register and login

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,5 @@
 from flask import Flask, escape, request, render_template, url_for
-
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '77f13060df47c6b4c5b3b2697aa27606'
@@ -28,6 +28,16 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title= 'About')
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title= 'Register', form= form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title= 'Login', form= form)
 
 if __name__ == '__main__':
     app.run(debug=True)
