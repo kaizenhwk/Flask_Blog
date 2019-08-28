49c41363b7d1d34275a0f9017f1b7abdf1145cfd
KaizenHwk <matin.mafuz@gmail.com>
2019-08-28 12:36:49 -0500

    chaged username length from 25 to 15

--- forms.py
+++ forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=25)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=15)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=3)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), Length(min=3), EqualTo('password')])
