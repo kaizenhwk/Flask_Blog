755a6254137354b9bf400e6bafc3fb739fce3865
KaizenHwk <matin.mafuz@gmail.com>
2019-10-30 00:46:43 -0500

    cache

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,11 +2,13 @@
 from flask import Flask, escape, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '77f13060df47c6b4c5b3b2697aa27606'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from flaskblog import routes
